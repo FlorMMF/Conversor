95a31069a95c8c5bcd8bb2dc8cdabcb5bc55631d
FlorM <flowy6290@gmail.com>
2025-01-06 14:53:28 -0700

    antes de poner la calculadora

---
+++ index.js
@@ -0,0 +1,12 @@
+const numInicial = document.getElementById('numInicial').value;
+const baseInicial = document.getElementById('baseInicial').value;
+const baseFinal = document.getElementById('baseFinal').value;
+const checkBtnEl = document.querySelector("button");
+
+
+
+
+checkBtnEl.addEventListener("click", () => {
+    
+   
+});
